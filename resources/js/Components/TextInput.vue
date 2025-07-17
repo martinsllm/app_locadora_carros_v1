7eacb34d1b7e6e9ee8b3dc946516bd7c89a023e0
martinsllm <lucaslm2010@gmail.com>
2025-07-17 20:48:30 -0300

    Implementing register of 'Marcas'

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -19,7 +19,7 @@ defineExpose({ focus: () => input.value.focus() });
 
 <template>
     <input
-        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded-md py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
         v-model="model"
         ref="input"
     />
--- resources/js/Pages/Marcas.vue
+++ resources/js/Pages/Marcas.vue
@@ -6,7 +6,7 @@ import Card from '@/Layouts/Card.vue';
 import Header from '@/Layouts/Header.vue';
 import Table from '@/Layouts/Table.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, useForm } from '@inertiajs/vue3';
 import { ref } from 'vue';
 import Modal from '@/Layouts/Modal.vue';
 
@@ -15,6 +15,38 @@ const showModal = ref(false)
 function toggleModal() {
     showModal.value = !showModal.value
 }
+
+const form = useForm({
+    nome: '',
+    imagem: [],
+});
+
+const submit = () => {
+    let urlBase = 'http://localhost:8000/api/v1/marca'
+
+    let formData = new FormData()
+    formData.append('nome', form.nome)
+    formData.append('imagem', form.imagem[0])
+
+    let config = {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+            'Accept': 'application/json'
+        }
+    }
+    
+    axios.post(urlBase, formData, config)
+        .then(response => {
+            console.log(response)
+        }).catch(errors => {
+            console.log(errors)
+        })
+};
+
+const carregarImagem = (e) => {
+    form.imagem = e.target.files
+}
+
 </script>
 
 
@@ -29,13 +61,13 @@ function toggleModal() {
 
                 <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                     <InputLabel>ID</InputLabel>
-                    <TextInput class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="ID" />
+                    <TextInput type="text" placeholder="ID"/>
                     <p class="text-xs italic">Opcional. Informe o ID do registro</p>
                 </div>
 
                 <div class="w-full md:w-1/2 px-3">
                     <InputLabel>Marca</InputLabel>
-                    <TextInput class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Nome da marca" />
+                    <TextInput type="text" placeholder="Nome da marca" />
                     <p class="text-xs italic">Opcional. Informe o nome da marca</p>
                 </div>
 
@@ -57,17 +89,17 @@ function toggleModal() {
         <Modal v-if="showModal" titulo="Nova Marca">
             <form>
                 <InputLabel class="mt-2">Nome</InputLabel>
-                <TextInput type="text" placeholder="Nome da marca" />
+                <TextInput type="text" placeholder="Nome da marca" v-model="form.nome"/>
                 
                 <InputLabel class="mt-2">Imagem</InputLabel>
-                <TextInput type="file" placeholder="Imagem" />
+                <TextInput type="file" placeholder="Imagem" @change="carregarImagem($event)"/>
                 <p class="text-xs italic">Selecione no formato PNG ou JPG</p>
 
                 <div class="flex shrink-0 flex-wrap items-center pt-4 justify-end">
                     <button @click="toggleModal" class="rounded-md border border-transparent py-2 px-4 text-center text-sm transition-all text-slate-600 hover:bg-slate-100 focus:bg-slate-100 active:bg-slate-100 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
                         Cancelar
                     </button>
-                    <button class="rounded-md bg-green-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-green-700 focus:shadow-none active:bg-green-700 hover:bg-green-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2" type="button">
+                    <button @click="submit" class="rounded-md bg-green-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-green-700 focus:shadow-none active:bg-green-700 hover:bg-green-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2" type="button">
                         Confirmar
                     </button>
                 </div>    
