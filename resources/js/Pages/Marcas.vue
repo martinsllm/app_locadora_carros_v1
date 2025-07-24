6d40b8169c5ac2bc5f19ddff169bdfeb5cdb840d
martinsllm <lucaslm2010@gmail.com>
2025-07-23 23:33:18 -0300

    Removing a register

--- resources/js/Pages/Marcas.vue
+++ resources/js/Pages/Marcas.vue
@@ -12,6 +12,7 @@ import { Head, useForm } from '@inertiajs/vue3';
 import { onMounted, reactive, ref } from 'vue';
 import Modal from '@/Layouts/Modal.vue';
 import { useStore } from 'vuex';
+import axios from 'axios';
 
 const showModal = ref(false)
 const store = useStore();
@@ -30,11 +31,6 @@ const marcas = ref([])
 const busca = reactive({
     id: '',
     nome: ''
-})
-
-const modais = reactive({
-    modal1: false,
-    modal2: false
 })
 
 function openModal(id) {
@@ -147,6 +143,28 @@ const pesquisar = () => {
     buscarLista()
 }
 
+function remover() {
+    let url = urlBase.value + '/' + store.state.item.id
+
+    let formData = new FormData()
+    formData.append('_method', 'delete')
+
+    let config = {
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': getToken()
+        }
+    }
+
+    axios.post(url, formData, config)
+        .then(() => {
+            buscarLista()
+            window.location.reload()
+        }).catch(errors => {
+            console.log(errors)
+        })
+}
+
 
 onMounted(() => {
     buscarLista()
@@ -249,14 +267,29 @@ onMounted(() => {
 
         <Modal v-if="store.state.openModal && store.state.modalId === 'view'" titulo="Visualizar">
             <InputLabel>ID</InputLabel>
-            <TextInput type="text" v-model="$store.state.item.id" disabled/>
+            <TextInput type="text" v-model="store.state.item.id" disabled/>
 
             <InputLabel class="mt-2">Nome</InputLabel>
-            <TextInput type="text" v-model="$store.state.item.nome" disabled/>
+            <TextInput type="text" v-model="store.state.item.nome" disabled/>
 
             <div class="flex shrink-0 flex-wrap items-center pt-4 justify-end">
                 <button @click="closeModal()" class="rounded-md border border-transparent py-2 px-4 text-center text-sm transition-all text-slate-600 hover:bg-slate-100 focus:bg-slate-100 active:bg-slate-100 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
                     Fechar
+                </button>
+            </div>  
+        </Modal>
+
+        <Modal v-if="store.state.openModal && store.state.modalId === 'delete'" titulo="Remover Marca">
+            <div class="mt-2">
+                Deseja remover a marca {{ store.state.item.nome }}?
+            </div>
+
+            <div class="flex shrink-0 flex-wrap items-center pt-4 justify-end">
+                <button @click="closeModal()" class="rounded-md border border-transparent py-2 px-4 text-center text-sm transition-all text-slate-600 hover:bg-slate-100 focus:bg-slate-100 active:bg-slate-100 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
+                    Cancelar
+                </button>
+                <button @click="remover()" class="rounded-md bg-red-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-red-700 focus:shadow-none active:bg-red-700 hover:bg-red-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2" type="button">
+                    Remover
                 </button>
             </div>  
         </Modal>
